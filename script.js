d35710134cdb663fa4eb44c869f0e6a6c6842ed6
Tạ Duy An <53089277+taduyan225@users.noreply.github.com>
2026-03-27 21:26:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+const chapters = [
+{
+title:"Chương 1: chap 1",
+date:"16-10-2025",
+link:"https://hihianankawaii.com/wp-admin/post.php?post=4367&action=edit"
+},
+{
+title:"Chương 2: chap 2",
+date:"12-11-2025",
+link:"https://hihianankawaii.com/wp-admin/post.php?post=4367&action=edit"
+}
+];
 
 const chapterList = document.getElementById("chapterList");
 
@@ -6,8 +20,12 @@ if(chapterList){
 
     chapters.forEach(chapter => {
         const a = document.createElement("a");
-        a.href = chapter.link;
+        a.href = "#";
         a.className = "chapter";
+
+        a.onclick = function() {
+            window.open(chapter.link, "_blank");
+        };
 
         a.innerHTML = `
             <span class="chapter-title">${chapter.title}</span>
